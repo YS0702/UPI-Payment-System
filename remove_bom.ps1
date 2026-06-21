16db26ac0b184f999e80703343baf6e78a0a17f0
Yash Srivastava <93868640+YS0702@users.noreply.github.com>
2026-06-21 22:26:17 +0530

    main code update

--- remove_bom.ps1
+++
@@ -1,8 +0,0 @@
-$paths = @('src\main\java', 'src\test\java')
-foreach ($path in $paths) {
-    Get-ChildItem -Path $path -Recurse -Filter '*.java' | ForEach-Object {
-        $content = Get-Content -Path $_.FullName -Raw
-        [System.IO.File]::WriteAllText($_.FullName, $content, (New-Object System.Text.UTF8Encoding($false)))
-        Write-Host "Cleaned BOM: $($_.FullName)"
-    }
-}
